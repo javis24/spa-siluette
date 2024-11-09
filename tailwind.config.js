e81dacbf60730d1d0f82aed79cc16c134b2a4f1f
Javier Mtz <luisjaviermtzb@gmail.com>
2024-11-08 21:59:44 -0600

    update-index-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}", // Incluye esta línea si usas App Router
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
